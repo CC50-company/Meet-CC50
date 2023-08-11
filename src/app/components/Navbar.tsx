74e55938e40ad923040637d95f0c239e9cb0d46e
João Victor Barbosa <joaovgb5@gmail.com>
2023-08-11 13:07:33 -0300

    #commit #3.2 navbar responsive

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 
 interface NavbarProps {
   links: { label: string; url: string }[];
 }
 
 const Navbar: React.FC<NavbarProps> = ({ links }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="bg-gray-0 p-4 lg:p-9">
       <div className="flex items-center justify-between">
         <div className="text-white font-bold text-lg md:text-3xl lg:text-4xl xl:text-5xl">CC50</div>
         {/* Menu responsivo para telas menores */}
-        <ul className="hidden lg:flex space-x-8">
+        <div className="lg:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <svg
+              className="h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+        </div>
+        {/* Menu para telas maiores */}
+        <ul className={`hidden lg:flex space-x-8 ${menuOpen ? 'block' : 'hidden'}`}>
           {links.map((link, index) => (
             <li key={index}>
-              {/* Aplicar classes CSS para animação no hover */}
               <a
                 href={link.url}
                 className="text-white text-md lg:text-lg xl:text-xl transition-colors duration-300 hover:text-gray-300"
@@ -23,13 +52,9 @@ const Navbar: React.FC<NavbarProps> = ({ links }) => {
             </li>
           ))}
         </ul>
-        {/* Ícone do menu responsivo para telas menores */}
-        <div className="lg:hidden">
-          
-        </div>
       </div>
       {/* Menu deslizável para telas menores */}
-      <div className="lg:hidden mt-4">
+      <div className={`lg:hidden mt-4 ${menuOpen ? 'block' : 'hidden'}`}>
         <ul className="space-y-2">
           {links.map((link, index) => (
             <li key={index}>
